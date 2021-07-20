e0bc2677076acd337093e93487f5de583ed50a03
RubioLin <capricorn1230maple@gmail.com>
2021-07-21 01:17:02 +0800

    #1

--- demo4/ViewController.swift
+++ demo4/ViewController.swift
@@ -7,13 +7,140 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let nameOfFriend = ["Winnie", "Piglet", "Eeyore", "Tigger"]
+let portraitsOfFriend = ["Winnie.png", "Piglet.png", "Eeyore.png", "Tigger.png"]
+var arrayIndex: Int = 0
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var name: UILabel!
+    @IBOutlet var portraitOfFriend: UIImageView!
+    @IBOutlet var pageDot: UIPageControl!
+    @IBOutlet var segment: UISegmentedControl!
+    
+    func sync() {
+        portraitOfFriend.image = UIImage(named: portraitsOfFriend[arrayIndex])
+        pageDot.currentPage = arrayIndex
+        name.text = nameOfFriend[arrayIndex]
+        segment.selectedSegmentIndex = arrayIndex
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        sync()
     }
 
-
+    @IBAction func pageControlChange(_ sender: Any) {
+        if pageDot.currentPage == 0 {
+            arrayIndex = 0
+            sync()
+        }
+        else if pageDot.currentPage == 1 {
+            arrayIndex = 1
+            sync()
+            
+        }
+        else if pageDot.currentPage == 2 {
+            arrayIndex = 2
+            sync()
+        }
+        else if pageDot.currentPage == 3 {
+            arrayIndex = 3
+            sync()
+        }
+    }
+    
+    @IBAction func nextButtonChange(_ sender: Any) {
+        if arrayIndex == 0 {
+            arrayIndex = arrayIndex + 1
+            sync()
+        }
+        else if arrayIndex == 1 {
+            arrayIndex = arrayIndex + 1
+            sync()
+        }
+        else if arrayIndex == 2 {
+            arrayIndex = arrayIndex + 1
+            sync()
+        }
+        else if arrayIndex == 3 {
+            arrayIndex = 0
+            sync()
+        }
+    }
+    
+    @IBAction func previousButtonChange(_ sender: Any) {
+        if arrayIndex == 0 {
+            arrayIndex = 3
+            sync()
+        }
+        else if arrayIndex == 1 {
+            arrayIndex = arrayIndex - 1
+            sync()
+        }
+        else if arrayIndex == 2 {
+            arrayIndex = arrayIndex - 1
+            sync()
+        }
+        else if arrayIndex == 3 {
+            arrayIndex = arrayIndex - 1
+            sync()
+        }
+    }
+    @IBAction func swipeToNextPic(_ sender: Any) {
+        if arrayIndex == 0 {
+            arrayIndex = arrayIndex + 1
+            sync()
+        }
+        else if arrayIndex == 1 {
+            arrayIndex = arrayIndex + 1
+            sync()
+        }
+        else if arrayIndex == 2 {
+            arrayIndex = arrayIndex + 1
+            sync()
+        }
+        else if arrayIndex == 3 {
+            arrayIndex = 0
+            sync()
+        }
+    }
+        
+    @IBAction func swipePreviousPic(_ sender: Any) {
+        if arrayIndex == 0 {
+            arrayIndex = 3
+            sync()
+        }
+        else if arrayIndex == 1 {
+            arrayIndex = arrayIndex - 1
+            sync()
+        }
+        else if arrayIndex == 2 {
+            arrayIndex = arrayIndex - 1
+            sync()
+        }
+        else if arrayIndex == 3 {
+            arrayIndex = arrayIndex - 1
+            sync()
+        }
+    }
+    @IBAction func segmentedControlChange(_ sender: Any) {
+        if segment.selectedSegmentIndex == 0 {
+            arrayIndex = 0
+            sync()
+        }
+        else if segment.selectedSegmentIndex == 1 {
+            arrayIndex = 1
+            sync()
+        }
+        else if segment.selectedSegmentIndex == 2 {
+            arrayIndex = 2
+            sync()
+        }
+        else if segment.selectedSegmentIndex == 3 {
+            arrayIndex = 3
+            sync()
+        }
+    }
+    
 }
-
